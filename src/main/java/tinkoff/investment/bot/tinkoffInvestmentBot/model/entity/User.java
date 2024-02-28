fa8b9ad2901a1d7ef635a6a847e68c20c48873f7
kostet <kostagarifullin275@gmail.com>
2024-02-28 23:30:54 +0500

    Add PostgreSQL with JDBC driver, docker compose file, user model. Getting ready for flyway implementation

---
+++ src/main/java/tinkoff/investment/bot/tinkoffInvestmentBot/model/entity/User.java
@@ -0,0 +1,16 @@
+package tinkoff.investment.bot.tinkoffInvestmentBot.model.entity;
+
+import lombok.*;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    private String tag;
+    private String chatId;
+}
